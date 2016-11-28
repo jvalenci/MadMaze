f850fb6c9825ba3b15f84e00a16cbcfd8baaabb7
jonathanValencia <jonathan.valencia716@gmail.com>
2016-11-28 18:05:39 -0500

    got rid of 3D array

--- src/MadMaze.java
+++ src/MadMaze.java
@@ -40,6 +40,13 @@ public class MadMaze {
             int levels = scan.nextInt();
             int rows = scan.nextInt();
             int cols = scan.nextInt();
+            int numOfVertices = levels * rows * cols;
+            int movingNorth = cols * -1;
+            int movingEast = 1;
+            int movingSouth = cols;
+            int movingWest = -1;
+            int movingUp = rows * cols;
+            int movingDown = -1 * rows * cols;
 
 
             //the start point
@@ -55,54 +62,43 @@ public class MadMaze {
             //my weighted graph
             SimpleDirectedWeightedGraph<Integer, DefaultWeightedEdge> graph = new SimpleDirectedWeightedGraph(DefaultWeightedEdge.class);
 
-            //init the 3D array and add vertices to graph
-            int [][][] maze = new int[levels][rows][cols];
-            int vertex = 0;
-            for(int j = 0; j < levels; j++){
-                for(int k = 0; k < rows; k++){
-                    for (int l = 0 ; l < cols; l++){
-                        ++vertex;
-                        maze[j][k][l] = vertex;
-                        graph.addVertex(maze[j][k][l]);
-                    }
-                }
+            //init vertices to graph
+            for(int v = 0; v < numOfVertices; v++) {
+                graph.addVertex(v);
             }
 
+
             //add the neighbors
-            for(int j = 0; j < levels; j++){
-                for(int k = 0; k < rows; k++){
-                    for (int l = 0 ; l < cols; l++){
-                        String vertNeighbors = scan.next();
-                        DefaultWeightedEdge edge;
-                        for(int c = 0; c < vertNeighbors.length(); c++){
-                            if( vertNeighbors.charAt(c) == '1'){
-                                switch(c){
-                                    case 0:
-                                        edge = graph.addEdge(maze[j][k][l], maze[j][k-1][l]);
-                                        graph.setEdgeWeight(edge, 1);
-                                        break;
-                                    case 1:
-                                        edge = graph.addEdge(maze[j][k][l], maze[j][k][l+1]);
-                                        graph.setEdgeWeight(edge, 1);
-                                        break;
-                                    case 2:
-                                        edge = graph.addEdge(maze[j][k][l], maze[j][k+1][l]);
-                                        graph.setEdgeWeight(edge, 1);
-                                        break;
-                                    case 3:
-                                        edge = graph.addEdge(maze[j][k][l], maze[j][k][l-1]);
-                                        graph.setEdgeWeight(edge, 1);
-                                        break;
-                                    case 4:
-                                        edge = graph.addEdge(maze[j][k][l], maze[j+1][k][l]);
-                                        graph.setEdgeWeight(edge, 1);
-                                        break;
-                                    case 5:
-                                        edge = graph.addEdge(maze[j][k][l], maze[j-1][k][l]);
-                                        graph.setEdgeWeight(edge, 1);
-                                        break;
-                                }
-                            }
+            for(int j = 0; j < numOfVertices; j++) {
+                String vertNeighbors = scan.next();
+                DefaultWeightedEdge edge;
+                for (int c = 0; c < vertNeighbors.length(); c++) {
+                    if (vertNeighbors.charAt(c) == '1') {
+                        switch (c) {
+                            case 0:
+                                edge = graph.addEdge(maze[j][k][l], maze[j][k - 1][l]);
+                                graph.setEdgeWeight(edge, 1);
+                                break;
+                            case 1:
+                                edge = graph.addEdge(maze[j][k][l], maze[j][k][l + 1]);
+                                graph.setEdgeWeight(edge, 1);
+                                break;
+                            case 2:
+                                edge = graph.addEdge(maze[j][k][l], maze[j][k + 1][l]);
+                                graph.setEdgeWeight(edge, 1);
+                                break;
+                            case 3:
+                                edge = graph.addEdge(maze[j][k][l], maze[j][k][l - 1]);
+                                graph.setEdgeWeight(edge, 1);
+                                break;
+                            case 4:
+                                edge = graph.addEdge(maze[j][k][l], maze[j + 1][k][l]);
+                                graph.setEdgeWeight(edge, 1);
+                                break;
+                            case 5:
+                                edge = graph.addEdge(maze[j][k][l], maze[j - 1][k][l]);
+                                graph.setEdgeWeight(edge, 1);
+                                break;
                         }
                     }
                 }
